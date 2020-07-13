1fe34163f1036bf822034f488fe5ead6027d0b7c
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-13 23:25:26 +0530

    Add files via upload

---
+++ LinkedinAddon.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using G1ANT.Language;
+using G1ANT.Addon.Linkedin.Properties;
+using System.IO;
+
+namespace G1ANT.Addon.Linkedin
+{
+    [Addon(Name = "Linkedin", Tooltip = "Automates the LinkedIn site")]
+    [Copyright(Author = "", Copyright = "", Email = "support@g1ant.com", Website = "www.g1ant.com")]
+    [License(Type = "LGPL", ResourceName = "License.txt")]
+    [CommandGroup(Name = "linkedin", Tooltip = "")]
+    public class LinkedinAddon : Language.Addon
+    {
+
+        public override void LoadDlls()
+        {
+            UnpackDrivers();
+            base.LoadDlls();
+        }
+
+        private void UnpackDrivers()
+        {
+            var unpackFolder = AbstractSettingsContainer.Instance.UserDocsAddonFolder.FullName;
+            var embeddedResourceDictionary = new Dictionary<string, byte[]>()
+            {
+                { "chromedriver.exe", Resources.chromedriver },
+                { "geckodriver.exe", Resources.geckodriver },
+                { "IEDriverServer.exe", Resources.IEDriverServer },
+                { "MicrosoftWebDriver.exe", Resources.MicrosoftWebDriver }
+            };
+            foreach (var embededResource in embeddedResourceDictionary.Where(e => !DoesFileExist(unpackFolder, e.Key) || !AreFilesOfTheSameLength(e.Value.Length, unpackFolder, e.Key)))
+            {
+                try
+                {
+                    using (FileStream stream = File.Create(Path.Combine(unpackFolder, embededResource.Key)))
+                    {
+                        stream.Write(embededResource.Value, 0, embededResource.Value.Length);
+                    }
+                }
+                catch (Exception ex) { RobotMessageBox.Show(ex.Message); }
+            }
+        }
+
+        private bool DoesFileExist(string folder, string fileName)
+        {
+            return File.Exists(Path.Combine(folder, fileName));
+        }
+
+        private bool AreFilesOfTheSameLength(int length, string folder, string fileName)
+        {
+            return length == new FileInfo(Path.Combine(folder, fileName)).Length;
+        }
+    }
+}
---
+++ LinkedinSearchCommand.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Linkedin
+{
+    [Command(Name = "linkedin.search", Tooltip = "This command is used to search on the linkedin site.")]
+    public class LinkedinSearchCommand : Command
+    {
+        public LinkedinSearchCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the value to search here")]
+            public TextStructure searchthis { get; set; }
+
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+
+                arguments.Search.Value = "search-global-typeahead__input";
+                arguments.By.Value = "class";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.TypeText(arguments.searchthis.Value, arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
