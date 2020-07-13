f3fcc02af2f7361f56e0750ee504297e80edc689
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-14 00:19:18 +0530

    Add files via upload

---
+++ Facebook Addon/FacebookAddon.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using G1ANT.Language;
+using System.IO;
+using G1ANT.Addon.Facebook.Properties;
+
+// Please remember to refresh G1ANT.Language.dll in references
+
+namespace G1ANT.Addon.Facebook
+{
+    [Addon(Name = "Facebook", Tooltip = "Commands to Automate Facebook")]
+    [Copyright(Author = "", Copyright = "", Email = "support@g1ant.com", Website = "www.g1ant.com")]
+    [License(Type = "LGPL", ResourceName = "License.txt")]
+    [CommandGroup(Name = "facebook", Tooltip = "...")]
+    public class FacebookAddon : Language.Addon
+    {
+        public override void LoadDlls()
+        {
+            UnpackDrivers();
+            base.LoadDlls();
+        }
+
+        private void UnpackDrivers()
+        {
+            var unpackFolder = AbstractSettingsContainer.Instance.UserDocsAddonFolder.FullName;
+            var embeddedResourceDictionary = new Dictionary<string, byte[]>()
+            {
+                { "chromedriver.exe", Resources.chromedriver },
+                { "geckodriver.exe", Resources.geckodriver },
+                { "IEDriverServer.exe", Resources.IEDriverServer },
+                { "MicrosoftWebDriver.exe", Resources.MicrosoftWebDriver }
+            };
+            foreach (var embededResource in embeddedResourceDictionary.Where(e => !DoesFileExist(unpackFolder, e.Key) || !AreFilesOfTheSameLength(e.Value.Length, unpackFolder, e.Key)))
+            {
+                try
+                {
+                    using (FileStream stream = File.Create(Path.Combine(unpackFolder, embededResource.Key)))
+                    {
+                        stream.Write(embededResource.Value, 0, embededResource.Value.Length);
+                    }
+                }
+                catch (Exception ex) { RobotMessageBox.Show(ex.Message); }
+            }
+        }
+
+        private bool DoesFileExist(string folder, string fileName)
+        {
+            return File.Exists(Path.Combine(folder, fileName));
+        }
+
+        private bool AreFilesOfTheSameLength(int length, string folder, string fileName)
+        {
+            return length == new FileInfo(Path.Combine(folder, fileName)).Length;
+        }
+    }
+}
