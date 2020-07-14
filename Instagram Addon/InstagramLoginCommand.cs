4d35bcfe6da74a9ffaeb8c77335e026e5f8fab52
Prajakta-Kamble <67152975+Prajakta-Kamble@users.noreply.github.com>
2020-07-14 15:25:39 +0530

    Add files via upload

---
+++ Instagram Addon/InstagramLoginCommand.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Instagram
+{
+    [Command(Name = "instagram.login", Tooltip = "This command will log you in instagram")]
+    class InstagramLoginCommand : Command
+    {
+        public InstagramLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : InstagramCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the Login e-mail ID here")]
+            public TextStructure email { get; set; }
+
+            [Argument(Required = true, Tooltip = "Enter the password here")]
+            public TextStructure pword { get; set; }
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        "chrome",
+                        "instagram.com",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                arguments.Search.Value = "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "username";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.email.Value, arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "password";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pword.Value, arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Instagram Addon/InstagramMakepostCommand.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Instagram
+{
+    [Command(Name = "instagram.makepost", Tooltip = "This command will post on instagram")]
+    class InstagramMakepostCommand : Command
+    {
+        public InstagramMakepostCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : InstagramCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the caption to post")]
+            public TextStructure caption { get; set; }
+            [Argument(Required = true, Tooltip = "Enter the image name to post")]
+            public TextStructure imagename { get; set; }
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        "chrome",
+                        "instagram.com",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+                arguments.Search.Value = "/html/body/div[1]/section/nav[2]/div/div/div[2]/div/div/div[3]/svg";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "filename";
+                arguments.By.Value = "id";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.imagename.Value, arguments, arguments.Timeout.Value);
+
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[1]/section/div[1]/header/div/div[2]/button";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/section/div[2]/section[1]/div[1]/textarea";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.caption.Value, arguments, arguments.Timeout.Value);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
