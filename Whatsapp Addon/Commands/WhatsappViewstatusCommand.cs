a0632f31c8c18058cf1afb63e0168157d85eac7e
Prajakta-Kamble <67152975+Prajakta-Kamble@users.noreply.github.com>
2020-07-15 20:45:59 +0530

    Add files via upload

---
+++ Whatsapp Addon/Commands/WhatsappViewstatusCommand.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Whatsapp
+{
+    [Command(Name = "whatsapp.viewstatus", Tooltip = "This command will view status on whatsapp")]
+    class WhatsappViewstatusCommand : Command
+    {
+        public WhatsappViewstatusCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : WhatsappCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[1]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
