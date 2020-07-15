f2e7ef432f41b67f3feb4936808eb79f0c50dab3
abhiakhi06 <67225463+abhiakhi06@users.noreply.github.com>
2020-07-15 19:06:05 +0530

    Add files via upload

---
+++ Twitter Addon/API/TimeoutSeleniumVariable.cs
@@ -0,0 +1,28 @@
+﻿using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.Twitter.Variables
+{
+    [Variable(
+        Name = "timeouttwitter",
+        Tooltip = "Determines the timeout value (in ms) for several `selenium.` commands; the default value is 10000 (10 seconds)")]
+    public class TimeoutTwitterVariable : Variable
+    {
+        private TimeSpanStructure value;
+
+        public TimeoutTwitterVariable(AbstractScripter scripter = null) : base(scripter) 
+        {
+            value = new TimeSpanStructure(new TimeSpan(0, 0, 10), "", scripter);
+        }
+
+        public override Structure GetValue(string index = null)
+        {
+            return value.Get(index);
+        }
+
+        public override void SetValue(Structure value, string index = null)
+        {
+            this.value.Set(value, index);
+        }
+    }
+}
