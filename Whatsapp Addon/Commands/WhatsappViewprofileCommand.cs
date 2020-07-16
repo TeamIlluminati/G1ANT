0201a99492018d36e8afb40c7aef8fb0668cbd2c
Prajakta-Kamble <67152975+Prajakta-Kamble@users.noreply.github.com>
2020-07-16 10:39:54 +0530

    Add files via upload

---
+++ Whatsapp Addon/Commands/WhatsappViewprofileCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Whatsapp
+{
+    [Command(Name = "whatsapp.viewprofile", Tooltip = "This command will view your profile on whatsapp")]
+    class WhatsappViewprofileCommand : Command
+    {
+        public WhatsappViewprofileCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : WhatsappCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[3]/div/header/div[2]/div/span/div[3]/span/div/ul/li[2]/div";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
