181dfb89799f8fcfc5db98c2a0020dbe0e016d5b
Prajakta-Kamble <67152975+Prajakta-Kamble@users.noreply.github.com>
2020-07-16 10:30:54 +0530

    Add files via upload

---
+++ Instagram Addon/Commands/InstagramOpensettingsCommand.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Instagram
+{
+    [Command(Name = "instagram.opensettings", Tooltip = "This command will open settings on instagram")]
+    class InstagramOpensettingsCommand : Command
+    {
+        public InstagramOpensettingsCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : InstagramCommandArguments
+        {
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/span/img";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+
+                arguments.Search.Value = "/html/body/div[1]/section/nav/div[2]/div/div/div[3]/div/div[5]/div[2]/div/div[2]/div[2]/a[3]/div/div[2]/div/div/div/div";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
