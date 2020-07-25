54666eeb5841380fce4a9643607d5dc2997a273d
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-25 23:59:18 +0530

    Add files via upload

---
+++ IRCTC Addon/Commands/IRCTCOpenCommand.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+
+namespace G1ANT.Addon.IRCTC
+{
+    [Command(Name = "irctc.open", Tooltip = "This command opens irctc website in user's specified browser")]
+    public class IRCTCOpenCommand : Command
+    {
+        public IRCTCOpenCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Name of a web browser")]
+            public TextStructure Type { get; set; } = new TextStructure("chrome");
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            [Argument(Tooltip = "By default, waits until the webpage fully loads")]
+            public BooleanStructure NoWait { get; set; } = new BooleanStructure(false);
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        arguments.Type.Value,
+                        "https://www.irctc.co.in/",
+                        arguments.Timeout.Value,
+                        arguments.NoWait.Value,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+
+                var len = SeleniumManager.CurrentWrapper.RunScript("return document.getElementsByClassName(\"captcha -internal\").length");
+                if (len == "1")
+                {
+                    RobotMessageBox.Show("Captcha detected, please solve the captcha");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+
+
+}
