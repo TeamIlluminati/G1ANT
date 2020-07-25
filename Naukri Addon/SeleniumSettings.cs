99706fab9c50487a3bf59b2a4a5f3ec499793507
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-26 00:04:57 +0530

    Add files via upload

---
+++ Naukri Addon/SeleniumSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Naukri
+{
+    public static class SeleniumSettings
+    {
+        public static int SeleniumTimeout = 20000;
+    }
+}
