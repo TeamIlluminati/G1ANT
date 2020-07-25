d092d88dd9bddb86962cef21da31a73d87999690
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-25 23:50:23 +0530

    Add files via upload

---
+++ Flipkart Addon/FlipkartLoginCommand.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Flipkart
+{
+    [Command(Name = "flipkart.login", Tooltip = "This command login into user's flipkart account.")]
+    public class FlipkartLoginCommand : Command
+    {
+        public FlipkartLoginCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Enter the Mobile number here")]
+            public TextStructure phoneno { get; set; }
+            [Argument(Required = true, Tooltip = "Enter the password here")]
+            public TextStructure pword { get; set; }
+
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after clicking the specified element")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div[2]/div/div/div/div/div[2]/div/form/div[1]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.phoneno.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[2]/div/div/div/div/div[2]/div/form/div[2]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pword.Value, arguments, arguments.Timeout.Value);
+                Thread.Sleep(3000);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+               
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Flipkart Addon/FlipkartSearchCommand.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Flipkart
+{
+    [Command(Name = "flipkart.search", Tooltip = "This command search the given product on flipkart.com")]
+    class FlipkartSearchCommand : Command
+    {
+        public FlipkartSearchCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Product to be searched")]
+            public TextStructure prod { get; set; }
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div/div/div[1]/div[1]/div[2]/div[2]/form/div/div/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.prod.Value, arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while typing text to element. Text: '{arguments.prod.Value}'. 'Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
