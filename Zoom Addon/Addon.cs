5d8be44451d2f0bffcdc40c1879421928cf710ff
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-26 00:30:45 +0530

    Add files via upload

--- Zoom Addon/Addon.cs
+++ Zoom Addon/Addon.cs
@@ -3,44 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using G1ANT.Language;
-
+using System.IO;
+using G1ANT.Addon.Zoom.Properties;
 // Please remember to refresh G1ANT.Language.dll in references
 
 namespace G1ANT.Addon.Zoom
 {
-    [Addon(Name = "Zoom", Tooltip = "Addon to automate zoom")]
-    [Copyright(Author = "G1ant Ltd", Copyright = "G1ant Ltd", Email = "support@g1ant.com", Website = "www.g1ant.com")]
+    [Addon(Name = "zoom", Tooltip = "Addon to automate zoom website")]
+    [Copyright(Author = "G1ANT Ltd", Copyright = "G1ant Ltd", Email = "hi@g1ant.com", Website = "www.g1ant.com")]
     [License(Type = "LGPL", ResourceName = "License.txt")]
-    //[CommandGroup(Name = "", Tooltip = "")]
     public class Addon : Language.Addon
     {
-
-        public override void Check()
-        {
-            base.Check();
-            // Check integrity of your Addon
-            // Throw exception if this Addon needs something that doesn't exists
-        }
-
         public override void LoadDlls()
         {
+            UnpackDrivers();
             base.LoadDlls();
-            // All dlls embeded in resources will be loaded automatically,
-            // but you can load here some additional dlls:
-
-            // Assembly.Load("...")
         }
 
-        public override void Initialize()
+        private void UnpackDrivers()
         {
-            base.Initialize();
-            // Insert some code here to initialize Addon's objects
+            var unpackFolder = AbstractSettingsContainer.Instance.UserDocsAddonFolder.FullName;
+            var embeddedResourceDictionary = new Dictionary<string, byte[]>()
+            {
+                { "chromedriver.exe", Resources.chromedriver },
+                { "geckodriver.exe", Resources.geckodriver },
+                { "IEDriverServer.exe", Resources.IEDriverServer },
+                { "MicrosoftWebDriver.exe", Resources.MicrosoftWebDriver }
+            };
+            foreach (var embededResource in embeddedResourceDictionary.Where(e => !DoesFileExist(unpackFolder, e.Key) || !AreFilesOfTheSameLength(e.Value.Length, unpackFolder, e.Key)))
+            {
+                try
+                {
+                    using (FileStream stream = File.Create(Path.Combine(unpackFolder, embededResource.Key)))
+                    {
+                        stream.Write(embededResource.Value, 0, embededResource.Value.Length);
+                    }
+                }
+                catch (Exception ex) { RobotMessageBox.Show(ex.Message); }
+            }
         }
 
-        public override void Dispose()
+        private bool DoesFileExist(string folder, string fileName)
         {
-            base.Dispose();
-            // Insert some code here which will dispose all unecessary objects when this Addon will be unloaded
+            return File.Exists(Path.Combine(folder, fileName));
+        }
+
+        private bool AreFilesOfTheSameLength(int length, string folder, string fileName)
+        {
+            return length == new FileInfo(Path.Combine(folder, fileName)).Length;
         }
     }
 }
