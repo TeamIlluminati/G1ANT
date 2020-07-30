955a884fc1a8c178fbe2efa1c3205622db2cc996
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-31 01:23:04 +0530

    Add files via upload

---
+++ Shaadi Addon/Commands/Shaadi.com_login.cs
@@ -0,0 +1,57 @@
+/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Selenium
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+
+using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.shaadi
+{
+    [Command(Name = "shaadi.com.login", Tooltip = "This command is used to login")]
+    public class ShaadiloginCommand : Command
+    {
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after clicking the specified element")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public  override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            [Argument(Required = true, Tooltip = "Enter the trend to be opened")]
+            public TextStructure username { get; set; } = new TextStructure();
+            [Argument(Required = true, Tooltip = "Enter the trend to be opened")]
+            public TextStructure pword { get; set; } = new TextStructure();
+        }
+        public ShaadiloginCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div[2]/div/div[1]/div[1]/div[2]/a[1]";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                arguments.Search.Value = "/html/body/div[2]/div/div[7]/form/div[2]/div[2]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.username.Value, arguments, arguments.Timeout.Value);
+                arguments.Search.Value = "/html/body/div[2]/div/div[7]/form/div[2]/div[3]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.pword.Value, arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while clicking element. Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
