02a9a87915f5d0461ddb72c39cbb336e7ab180e2
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-07-31 01:30:58 +0530

    Add files via upload

---
+++ Uber Addon/Commands/Uberphone.cs
@@ -0,0 +1,54 @@
+/**
+*    Copyright(C) G1ANT Ltd, All rights reserved
+*    Solution G1ANT.Addon, Project G1ANT.Addon.Selenium
+*    www.g1ant.com
+*
+*    Licensed under the G1ANT license.
+*    See License.txt file in the project root for full license information.
+*
+*/
+
+using G1ANT.Language;
+using System;
+
+namespace G1ANT.Addon.uber
+{
+    [Command(Name = "uber.phone", Tooltip = "This command types the phone number to login")]
+    public class UberloginphoneCommand : Command
+    {
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Tooltip = "If set to `true`, the command should wait for a new window to appear after clicking the specified element")]
+            public BooleanStructure WaitForNewWindow { get; set; } = new BooleanStructure(false);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public  override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            [Argument(Required = true, Tooltip = "Enter the trend to be opened")]
+            public TextStructure phno { get; set; } = new TextStructure();
+            
+        }
+        public UberloginphoneCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div/div/div/div[1]/main/nav/div/ul[3]/li[1]/button/svg";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                arguments.Search.Value = "/html/body/div[1]/div/div/div[2]/div/div[3]/div/div[2]/div/section/div/div/div/div/div[3]/a/div[2]/h2";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value, arguments.WaitForNewWindow.Value);
+                arguments.Search.Value = "/html/body/div[1]/div/div/div/div/div/div/div/div[2]/div/form/div[1]/div/div[1]/div/div[2]/input";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.TypeText(arguments.phno.Value, arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.PressKey("enter", arguments, arguments.Timeout.Value);
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while clicking element. Search element phrase: '{arguments.Search.Value}', by: '{arguments.By.Value}'. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
