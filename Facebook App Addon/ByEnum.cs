a2381973cbfdcb2bf369112a3b2bdc8410ce2928
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-08-01 00:04:53 +0530

    Add files via upload

---
+++ Facebook App Addon/ByEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace G1ANT.Addon.FacebookApp
+{
+    public enum SearchBy
+    {
+        Id,
+        AccessibilityId,
+        Text,
+        PartialId,
+        Xpath,
+        XY
+    }
+}
