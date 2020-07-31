789c8e04f42bbd3798b0381f69a7ebd2946ff818
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-08-01 00:00:00 +0530

    Add files via upload

---
+++ Youtube App Addon/ByEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace G1ANT.Addon.YoutubeApp
+{
+    public enum SearchBy
+    {
+        Id,
+        AccessibilityId,
+        Text,
+        PartialId,
+        Xpath,
+        XY
+    }
+}
