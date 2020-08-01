2ddcc0ae690fea11a3d504d8bf3827e552f9b17b
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-08-02 01:33:06 +0530

    Add files via upload

--- Linkedin Addon/Commands/LinkedinPostCommand.cs
+++ Linkedin Addon/Commands/LinkedinPostCommand.cs
@@ -9,7 +9,7 @@ using G1ANT.Addon.Linkedin;
 
 namespace G1ANT.Addon.linked
 {
-    [Command(Name = "linkedin.post", Tooltip = "This command allows user to post on linkedin profile")]
+    [Command(Name = "linkedin.post", Tooltip = "This command allows user to post on linkedin ")]
     class LinkedinPostCommand : Command
     {
         public LinkedinPostCommand(AbstractScripter scripter) : base(scripter)
@@ -18,24 +18,22 @@ namespace G1ANT.Addon.linked
         }
         public class Arguments : SeleniumCommandArguments
         {
-            [Argument(Required = true, Tooltip = "Enter the value to be posted ")]
-            public TextStructure postvalue { get; set; }
+            [Argument(Required = true, Tooltip = "Enter the message to be posted ")]
+            public TextStructure topost { get; set; }
 
-
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
         }
         public void Execute(Arguments arguments)
         {
             try
             {
-                //arguments.Search.Value = "bug-text-color";
-                //arguments.By.Value = "class";
-                //SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
                 arguments.Search.Value = "/html/body/div[9]/div[4]/div/div/div/div/div[1]/div/div[1]/button";
                 arguments.By.Value = "xpath";
                 SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
                 arguments.Search.Value = "/html/body/div[4]/div/div/div[2]/div/div[1]/div[1]/div[2]/div/div/div/div/div[1]";
                 arguments.By.Value = "xpath";
-                SeleniumManager.CurrentWrapper.TypeText(arguments.postvalue.Value, arguments, arguments.Timeout.Value);
+                SeleniumManager.CurrentWrapper.TypeText(arguments.topost.Value, arguments, arguments.Timeout.Value);
 
                 arguments.Search.Value = "/html/body/div[4]/div/div/div[2]/div/div[2]/div[2]/button";
                 arguments.By.Value = "xpath";
