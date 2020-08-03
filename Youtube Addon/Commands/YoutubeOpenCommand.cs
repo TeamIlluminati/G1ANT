536c95de31e0d30d02ff8cd5a17f536247c983ec
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-08-04 00:11:18 +0530

    Update YoutubeOpenCommand.cs

--- Youtube Addon/Commands/YoutubeOpenCommand.cs
+++ Youtube Addon/Commands/YoutubeOpenCommand.cs
@@ -19,8 +19,7 @@ namespace G1ANT.Addon.Youtube
         public class Arguments : SeleniumCommandArguments
         {
             [Argument(Required = true, Tooltip = "Name of a web browser")]
-            public TextStructure type { get; set; } = new TextStructure(string.Empty);
-
+            public TextStructure type { get; set; } = new TextStructure("chrome");
             [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
             public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
             [Argument(Tooltip = "By default, waits until the webpage fully loads")]
