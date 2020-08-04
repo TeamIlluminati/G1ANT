7ba6e87a2742c59f3a77abe69d2fa24edce4ee47
Prajakta-Kamble <67152975+Prajakta-Kamble@users.noreply.github.com>
2020-08-04 14:29:23 +0530

    Add files via upload

---
+++ Ola App Addon/Commands/OlaAppBookCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using G1ANT.Addon.OlaApp;
+using G1ANT.Language;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.OlaApp
+{
+    [Command(Name = "olaapp.book", Tooltip = "This command will book a ride")]
+    public class OlaAppBookCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure(string.Empty);
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure(string.Empty);
+
+            [Argument(Required = true, Tooltip = "Specify your location")]
+            public TextStructure pick { get; set; } = new TextStructure("");
+
+            [Argument(Required = true, Tooltip = "Specify drop location")]
+            public TextStructure drop { get; set; } = new TextStructure("");
+        }
+
+        public OlaAppBookCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).Click();
+
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup/android.widget.EditText";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).Click();
+
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).SendKeys(arguments.pick.Value);
+
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup[2]/android.view.ViewGroup/android.widget.EditText";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).Click();
+
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).SendKeys(arguments.drop.Value);
+        }
+    }
+}
+
+
---
+++ Ola App Addon/Commands/OlaAppPaymentsCommand.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+using G1ANT.Addon.OlaApp;
+using G1ANT.Language;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Appium.Enums;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Appium.Service;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.OlaApp
+{
+    [Command(Name = "olaapp.payments", Tooltip = "This command opens your payment option")]
+    public class OlaAppPaymentsCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = false, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure(string.Empty);
+
+            [Argument(Required = false, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure(string.Empty);
+        }
+
+        public OlaAppPaymentsCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout[2]/android.widget.FrameLayout/android.view.ViewGroup/android.view.ViewGroup/android.widget.FrameLayout/android.view.ViewGroup/android.widget.ImageView";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).Click();
+
+            arguments.Search.Value = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/androidx.drawerlayout.widget.DrawerLayout/android.widget.FrameLayout[3]/androidx.recyclerview.widget.RecyclerView/androidx.appcompat.widget.LinearLayoutCompat[4]";
+            arguments.By.Value = "xpath";
+            ElementHelper.GetElement(arguments.By.Value, arguments.Search.Value).Click();
+        }
+    }
+}
+
+
