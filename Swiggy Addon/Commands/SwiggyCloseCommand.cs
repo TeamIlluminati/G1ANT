f36c2b47a8d3ef2e419c424c75bb5b4d00c7805e
Prakhar2211 <66748441+Prakhar2211@users.noreply.github.com>
2020-08-08 21:29:51 +0530

    Add files via upload

---
+++ Swiggy Addon/Commands/SwiggyCloseCommand.cs
@@ -0,0 +1,32 @@
+﻿using G1ANT.Language;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G1ANT.Addon.Swiggy
+{
+    [Command(Name = "swiggy.close", Tooltip = "This command closes a web browser")]
+    public class SwiggyCloseCommand : Command
+    {
+        public class Arguments : CommandArguments
+        {
+        }
+        public SwiggyCloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.QuitCurrentWrapper();
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while closing selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
---
+++ Swiggy Addon/Commands/SwiggyOpenCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using OpenQA.Selenium;
+
+namespace G1ANT.Addon.Swiggy
+{
+    [Command(Name = "swiggy.open", Tooltip = "This command will open naukri.com website in specfied web browser")]
+    public class SwiggyOpenCommand : Command
+    {
+        public SwiggyOpenCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : SeleniumCommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Name of a web browser")]
+            public TextStructure type { get; set; } = new TextStructure(string.Empty);
+
+            [Argument(DefaultVariable = "timeoutselenium", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+            
+
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumWrapper wrapper = SeleniumManager.CreateWrapper(
+                        arguments.type.Value,
+                        "swiggy.com",
+                        arguments.Timeout.Value,
+                        false,
+                        Scripter.Log,
+                        Scripter.Settings.UserDocsAddonFolder.FullName);
+                int wrapperId = wrapper.Id;
+                OnScriptEnd = () =>
+                {
+                    SeleniumManager.DisposeAllOpenedDrivers();
+                    SeleniumManager.RemoveWrapper(wrapperId);
+                    SeleniumManager.CleanUp();
+                };
+
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+
+    }
+}
