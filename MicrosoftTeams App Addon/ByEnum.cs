c840993cc4ccce57f6ace5410edb7bec1fbab012
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-09 11:50:01 +0530

    Add files via upload

---
+++ MicrosoftTeams App Addon/ByEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace G1ANT.Addon.MicrosoftTeamsAndroid
+{
+    public enum SearchBy
+    {
+        Id,
+        AccessibilityId,
+        Text,
+        PartialId,
+        Xpath,
+        XY
+    }
+}
