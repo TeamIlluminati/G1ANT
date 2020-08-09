f7d42ecae5ac3e8d7c5cf98de324928819026cf2
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-09 11:46:26 +0530

    Add files via upload

---
+++ Medium App Addon/ByEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace G1ANT.Addon.MediumAndroid 
+{
+    public enum SearchBy
+    {
+        Id,
+        AccessibilityId,
+        Text,
+        PartialId,
+        Xpath,
+        XY
+    }
+}
