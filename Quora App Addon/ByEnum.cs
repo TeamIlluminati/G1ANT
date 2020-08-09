cc62e7ed25f2f8d2909c598cc8870ce22e280d88
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-09 11:40:05 +0530

    Add files via upload

---
+++ Quora App Addon/ByEnum.cs
@@ -0,0 +1,12 @@
+﻿namespace G1ANT.Addon.QuoraAndroid 
+{
+    public enum SearchBy
+    {
+        Id,
+        AccessibilityId,
+        Text,
+        PartialId,
+        Xpath,
+        XY
+    }
+}
