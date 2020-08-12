050eb627c80d8318f559400a7fcb56fc157ea10e
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-12 13:32:55 +0530

    Add files via upload

---
+++ CiscoWebEx Addon App/AppiumCommandArguments.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Linq;
+using System.Text;
+using G1ANT.Language;
+
+
+namespace G1ANT.Addon.CiscoWebExAndroid
+{
+
+    public class AppiumCommandArguments : CommandArguments
+    {
+        // Enter all arguments you need 
+        [Argument(Required = false, Tooltip = "Provide name of the capability")]
+        public TextStructure Search { get; set; } = new TextStructure(string.Empty);
+
+        [Argument(Required = false, Tooltip = "Provide element ID")]
+        public VariableStructure Result { get; set; } = new VariableStructure(string.Empty);
+    }
+}
