82d954a9016ea89dcdc513adddcf3b74ccd9c217
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-14 19:40:59 +0530

    Add files via upload

---
+++ Medium/Commands/MediumCloseCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Medium
+{
+    [Command(Name = "medium.close", Tooltip = "This closes the instance of Medium")]
+    class MediumCloseCommand : Command
+    {
+        public MediumCloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : MediumCommandArguments 
+        {
+
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.QuitCurrentWrapper();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
