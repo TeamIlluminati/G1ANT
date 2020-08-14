80a4946636410ae6b5035452d25eb690ce63078b
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-14 19:43:51 +0530

    Add files via upload

---
+++ Quora Addon/Commands/QuoraCloseCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+
+namespace G1ANT.Addon.Quora
+{
+    [Command(Name = "quora.close", Tooltip = "This closes the instance of Quora Web Browser")]
+    class QuoraCloseCommand : Command
+    {
+        public QuoraCloseCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : QuoraCommandArguments
+        {
+
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                SeleniumManager.QuitCurrentWrapper();
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
