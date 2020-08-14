3fee30719f44d18d1b354190cb80374fc10c21b2
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-14 11:40:04 +0530

    Add files via upload

---
+++ Twitter Addon/Commands/TwitterViewProfileCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using G1ANT.Language;
+using G1ANT.Addon.Twitter;
+
+namespace G1ANT.Addon.Twitter
+{
+    [Command(Name = "twitter.viewprofile", Tooltip = "This helps a user view his/her profile on Twitter")]
+    class TwitterViewProfileCommand : Command
+    {
+        public TwitterViewProfileCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+        public class Arguments : TwitterCommandArguments
+        {
+            [Argument(DefaultVariable = "timeouttwitter", Tooltip = "Specifies time in milliseconds for G1ANT.Robot to wait for the command to be executed")]
+            public override TimeSpanStructure Timeout { get; set; } = new TimeSpanStructure(SeleniumSettings.SeleniumTimeout);
+        }
+        public void Execute(Arguments arguments)
+        {
+            try
+            {
+                arguments.Search.Value = "/html/body/div/div/div/div[2]/header/div/div/div/div[1]/div[2]/nav/a[7]/div";
+                arguments.By.Value = "xpath";
+                SeleniumManager.CurrentWrapper.Click(arguments, arguments.Timeout.Value);
+            }
+            catch(Exception ex)
+            {
+                throw new ApplicationException($"Error occured while opening new selenium instance. Message: {ex.Message}", ex);
+            }
+        }
+    }
+}
