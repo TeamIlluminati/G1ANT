adcae1b2a7a9ca94b35019755dbefb041b1f923e
Abhishek Krishna Swamy <67225463+abhiakhi06@users.noreply.github.com>
2020-08-15 10:04:57 +0530

    Add files via upload

---
+++ Uber App Addon/Commands/uberfood.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using G1ANT.Language;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Remote;
+
+namespace G1ANT.Addon.uber
+{
+    [Command(Name = "uber.food", Tooltip = "This command opens food page")]
+    public class FoodCommand : Language.Command
+    {
+        public class Arguments : CommandArguments
+        {
+            [Argument(Required = true, Tooltip = "Provide name of the capaility")]
+            public TextStructure Search { get; set; } = new TextStructure("");
+
+            [Argument(Required = true, Tooltip = "Provide element ID")]
+            public TextStructure By { get; set; } = new TextStructure("");
+        }
+
+        public FoodCommand(AbstractScripter scripter) : base(scripter)
+        {
+
+        }
+
+        public void Execute(Arguments arguments)
+        {
+            arguments.Search.Value = "/ hierarchy / android.widget.FrameLayout / android.widget.LinearLayout / android.widget.FrameLayout / android.widget.LinearLayout / android.widget.FrameLayout / android.view.ViewGroup / android.view.ViewGroup / android.view.ViewGroup / androidx.drawerlayout.widget.DrawerLayout / android.view.ViewGroup / androidx.recyclerview.widget.RecyclerView / android.widget.FrameLayout[2]";
+            arguments.By.Value = "xpath";
+            var by = arguments.By.Value.ToLower();
+
+            if(by == "xy")
+            {
+                TouchAction clickAction = new TouchAction(OpenCommand.GetDriver());
+                var coordinates = arguments.Search.Value.Split(',');
+                clickAction.Tap(int.Parse(coordinates[0]), int.Parse(coordinates[1])).Perform();
+            }
+            else
+            {
+                ElementHelper.GetElement(arguments.By.Value.ToLower(), arguments.Search.Value).Click();
+
+            }
+        }
+    }
+}
